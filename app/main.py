9a325e8dfdce00ea49cfeb7d0da35f02c098cd5b
Anton Fenske <anton@devskills.co>
2022-03-25 21:15:43 +0000

    Move to Flask

--- app/main.py
+++ app/main.py
@@ -1,37 +1,11 @@
-from typing import Optional
+from sanic import Sanic
+from sanic.response import json
 
-from fastapi import FastAPI
+app = Sanic("My Hello, world app")
 
-from databases import Database
+@app.route('/ping')
+async def test(request):
+    return json({'result': 'pong'})
 
-database = Database("sqlite:///app.db")
-
-app = FastAPI()
-
-# NOTE: Feel free to restructure the code and database schema the way you prefer. This is only an example.
-
-@app.on_event("startup")
-async def database_connect():
-    await database.connect()
-    query = """CREATE TABLE IF NOT EXISTS HighScores (id INTEGER PRIMARY KEY, name VARCHAR(100), score INTEGER)"""
-    await database.execute(query=query)
-
-    query = "INSERT INTO HighScores(name, score) VALUES (:name, :score)"
-    values = [
-        {"name": "Daisy", "score": 92},
-        {"name": "Neil", "score": 87},
-        {"name": "Carol", "score": 43},
-    ]
-    await database.execute_many(query=query, values=values)
-
-
-@app.on_event("shutdown")
-async def database_disconnect():
-    await database.disconnect()
-
-
-@app.get("/ping")
-async def ping():
-    query = "SELECT name,score FROM HighScores"
-    rows = await database.fetch_all(query=query)
-    return {"rows": rows}
+if __name__ == '__main__':
+    app.run()
